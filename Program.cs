94445ee8708aabeb9bb5319cb6c01c54a0c91b02
Fernanda Vitória <ferferreira929@gmail.com>
2024-11-01 20:45:38 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+ExibirTitulo();
+ 
+Console.Write("informe um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+ 
+bool ehPar = DefinirPar (numero);
+ 
+ 
+void ExibirTitulo()
+{ 
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("--------------------");
+Console.WriteLine("--- Par ou Impar ---");
+Console.WriteLine("--------------------\n");
+Console.ResetColor();
+}
+ 
+bool DefinirPar(int numero)
+{
+bool ehPar = numero % 2 == 0;
+return ehPar;
+}
+ 
+ExibirResultado();
+ 
+void ExibirResultado()
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine($"O número {numero} é {(ehPar ? "Par" : "impar")}");
+Console.WriteLine("-----------------");
+Console.WriteLine("--- Resultado ---");
+Console.WriteLine("------------------\n");
+Console.ResetColor();
+}
+tem menu de contexto
